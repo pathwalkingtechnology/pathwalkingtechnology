59c8d438de337739519df7ae48a0ab0313f60952
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-11-01 11:03:45 -0300

    Primer commit

---
+++ lib/gtag.ts
@@ -0,0 +1,23 @@
+// lib/gtag.ts
+export const GA_TRACKING_ID = 'G-1Q69KJEDRF';
+
+// Función para rastrear vistas de página
+export const pageview = (url: string) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+// Función para rastrear eventos
+export const event = ({ action, category, label, value }: { 
+  action: string; 
+  category: string; 
+  label: string; 
+  value: number;
+}) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google Analytics script */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
