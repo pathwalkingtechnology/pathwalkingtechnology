c12524b577a75ee3789fd43282cfb06ee0e70a88
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-11-01 15:17:26 -0300

    V 1.0.2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   env: {
-    GA_TRACKING_ID: process.env.GA_TRACKING_ID || 'G-1Q69KJEDRF',
+    NEXT_PUBLIC_GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID || 'G-1Q69KJEDRF',
   },
 };
 
