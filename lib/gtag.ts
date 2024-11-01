d3897f70908a42ebecd2013d8a6f3dbb424e0067
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-11-01 11:53:45 -0300

    V 1.0.1

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,16 +1,22 @@
-// lib/gtag.ts
-export const GA_TRACKING_ID = 'G-1Q69KJEDRF';
-
 declare global {
   interface Window {
-    gtag: (action: string, config?: { [key: string]: any }) => void;
-    dataLayer: any[];
+    gtag: (action: string, config?: { [key: string]: string | number | boolean }) => void;
+    dataLayer: (string | number | boolean)[];
   }
+}
+
+export const GA_TRACKING_ID = process.env.GA_TRACKING_ID;
+
+if (!GA_TRACKING_ID) {
+  throw new Error('GA_TRACKING_ID no está definido');
 }
 
 // Función para rastrear vistas de página
 export const pageview = (url: string) => {
-  window.gtag('config', GA_TRACKING_ID, {
+  window.gtag('config', { tracking_id: GA_TRACKING_ID });
+  window.gtag('event', {
+    action: 'pageview',
+    send_to: GA_TRACKING_ID,
     page_path: url,
   });
 };
@@ -22,9 +28,10 @@ export const event = ({ action, category, label, value }: {
   label: string; 
   value: number;
 }) => {
-  window.gtag('event', action, {
+  window.gtag('event', {
+    action,
     event_category: category,
     event_label: label,
-    value: value,
+    value,
   });
 };
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  env: {
+    GA_TRACKING_ID: process.env.GA_TRACKING_ID || 'G-1Q69KJEDRF',
+  },
 };
 
 export default nextConfig;
